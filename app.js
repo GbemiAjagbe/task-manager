6312602c8a8b1e1c73a8a308d97938c303561bbe
GbemiAjagbe <63325195+GbemiAjagbe@users.noreply.github.com>
2023-01-12 07:11:30 +0000

    database connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 
 //middleware
 app.use(express.json())
@@ -18,5 +21,15 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000
 
-app.listen(port, console.log(`server on port ${port}`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
 
